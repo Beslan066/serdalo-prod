e9d776cccc2c8ab154950d21132dbae6e29bb97f
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-11-26 21:17:33 +0300

    Файл роботов и сайтмап для новостей гугл

--- resources/views/frontend/v3/sitemaps/news.blade.php
+++ resources/views/frontend/v3/sitemaps/news.blade.php
@@ -4,21 +4,24 @@
         <url>
             <loc>https://serdalo.ru/{{ $post->slug }}</loc>
             <lastmod>{{ $post->updated_at->toIso8601String() }}</lastmod>
+
             <news:news>
                 <news:publication>
                     <news:name>Сердало</news:name>
                     <news:language>ru</news:language>
                 </news:publication>
                 <news:publication_date>{{ $post->published_at->format('Y-m-d\TH:i:s\Z') }}</news:publication_date>
-                <news:title>{{ $post->title }}</news:title>
+                <news:title>{{ htmlspecialchars($post->title) }}</news:title>
                 <news:keywords>{{ $post->tags->pluck('title')->implode(', ') }}</news:keywords>
-                @if($post->file)
-                    <image:image>
-                        <image:loc>{{ $post->file->full_preview_path }}</image:loc>
-                        <image:title>{{ $post->title }}</image:title>
-                    </image:image>
-                @endif
             </news:news>
+
+            @if($post->file)
+                <image:image>
+                    <image:loc>{{ $post->file->full_preview_path }}</image:loc>
+                    <image:title>{{ htmlspecialchars($post->title) }}</image:title>
+                </image:image>
+            @endif
+
         </url>
     @endforeach
 </urlset>
